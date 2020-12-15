24ab02d257c83f9aa7b6e8e60935d12d1d65c879
jackokring <jackokring@gmail.com>
2020-12-15 18:34:51 +0000

    lazurus too?

---
+++ Lazarus/amyguimain.pas
@@ -0,0 +1,39 @@
+unit AMyGUIMain;
+
+{$mode objfpc}{$H+}
+
+interface
+
+uses
+  Classes, SysUtils, Forms, Controls, Graphics, Dialogs, StdCtrls, Menus;
+
+type
+
+  { TMainForm }
+
+  TMainForm = class(TForm)
+    MainMenu: TMainMenu;
+    FileMenu: TMenuItem;
+    procedure FormCreate(Sender: TObject);
+  private
+
+  public
+
+  end;
+
+var
+  MainForm: TMainForm;
+
+implementation
+
+{$R *.lfm}
+
+{ TMainForm }
+
+procedure TMainForm.FormCreate(Sender: TObject);
+begin
+
+end;
+
+end.
+
---
+++ Lazarus/backup/amyguimain.pas
@@ -0,0 +1,38 @@
+unit AMyGUIMain;
+
+{$mode objfpc}{$H+}
+
+interface
+
+uses
+  Classes, SysUtils, Forms, Controls, Graphics, Dialogs, StdCtrls, Menus;
+
+type
+
+  { TMainForm }
+
+  TMainForm = class(TForm)
+    MainMenu: TMainMenu;
+    procedure FormCreate(Sender: TObject);
+  private
+
+  public
+
+  end;
+
+var
+  MainForm: TMainForm;
+
+implementation
+
+{$R *.lfm}
+
+{ TMainForm }
+
+procedure TMainForm.FormCreate(Sender: TObject);
+begin
+
+end;
+
+end.
+
