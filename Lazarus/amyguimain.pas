73d79b348137c173028a6b4d7c64c898aa827c8b
jackokring <jackokring@gmail.com>
2021-01-01 21:55:22 +0000

    UI

--- Lazarus/amyguimain.pas
+++ Lazarus/amyguimain.pas
@@ -14,10 +14,7 @@ type
 
   TMainForm = class(TForm)
     InsertTabMenuItem: TMenuItem;
-    RedoActionItem: TAction;
-    RedoButton: TButton;
-    ExecuteActionItem: TAction;
-    ActionList: TActionList;
+    InsertEnterMenuItem: TMenuItem;
     ExecuteButton: TButton;
     MainMenu: TMainMenu;
     FileMenu: TMenuItem;
@@ -26,26 +23,55 @@ type
     CopyMenuItem: TMenuItem;
     ExportMenuItem: TMenuItem;
     InputText: TMemo;
+    InsertReprintLineMenuItem: TMenuItem;
+    InsertReprintPageMenuItem: TMenuItem;
+    InsertColumnAlignMenuItem: TMenuItem;
+    InsertBackspaceMenuItem: TMenuItem;
+    ArchiveMenuItem: TMenuItem;
+    MakeNoteMenuItem: TMenuItem;
+    MarkMenuItem: TMenuItem;
+    RefreshMenuItem: TMenuItem;
+    QueryMenuItem: TMenuItem;
+    NotifyMenuItem: TMenuItem;
+    RecallMenuItem: TMenuItem;
+    SignVerifyMenuItem: TMenuItem;
+    ToolsMenu: TMenuItem;
+    TrayMenuItem: TMenuItem;
+    ViewMenu: TMenuItem;
+    QuitMenuItem: TMenuItem;
     OutputText: TMemo;
     SelectAllMenuItem: TMenuItem;
     PasteMenuItem: TMenuItem;
-    ViewMenu: TMenuItem;
     OpenMenuItem: TMenuItem;
     SaveMenuItem: TMenuItem;
     NewMenuItem: TMenuItem;
+    procedure ArchiveMenuItemClick(Sender: TObject);
     procedure CopyMenuItemClick(Sender: TObject);
     procedure CutMenuItemClick(Sender: TObject);
     procedure ExecuteActionItemExecute(Sender: TObject);
     procedure ExecuteButtonClick(Sender: TObject);
     procedure ExportMenuItemClick(Sender: TObject);
     procedure FormCreate(Sender: TObject);
+    procedure InsertBackspaceMenuItemClick(Sender: TObject);
+    procedure InsertEnterMenuItemClick(Sender: TObject);
+    procedure InsertReprintLineMenuItemClick(Sender: TObject);
+    procedure InsertReprintPageMenuItemClick(Sender: TObject);
     procedure InsertTabMenuItemClick(Sender: TObject);
+    procedure InsertColumnAlignMenuItemClick(Sender: TObject);
+    procedure MakeNoteMenuItemClick(Sender: TObject);
+    procedure MarkMenuItemClick(Sender: TObject);
+    procedure QueryMenuItemClick(Sender: TObject);
+    procedure NotifyMenuItemClick(Sender: TObject);
     procedure OpenMenuItemClick(Sender: TObject);
     procedure NewMenuItemClick(Sender: TObject);
     procedure PasteMenuItemClick(Sender: TObject);
+    procedure QuitMenuItemClick(Sender: TObject);
+    procedure RecallMenuItemClick(Sender: TObject);
     procedure RedoActionItemExecute(Sender: TObject);
     procedure SaveMenuItemClick(Sender: TObject);
     procedure SelectAllMenuItemClick(Sender: TObject);
+    procedure SignVerifyMenuItemClick(Sender: TObject);
+    procedure TrayMenuItemClick(Sender: TObject);
   private
 
   public
@@ -66,12 +92,62 @@ begin
 
 end;
 
+procedure TMainForm.InsertBackspaceMenuItemClick(Sender: TObject);
+begin
+
+end;
+
+procedure TMainForm.InsertEnterMenuItemClick(Sender: TObject);
+begin
+
+end;
+
+procedure TMainForm.InsertReprintLineMenuItemClick(Sender: TObject);
+begin
+
+end;
+
+procedure TMainForm.InsertReprintPageMenuItemClick(Sender: TObject);
+begin
+
+end;
+
 procedure TMainForm.InsertTabMenuItemClick(Sender: TObject);
 begin
 
 end;
 
+procedure TMainForm.InsertColumnAlignMenuItemClick(Sender: TObject);
+begin
+
+end;
+
+procedure TMainForm.MakeNoteMenuItemClick(Sender: TObject);
+begin
+
+end;
+
+procedure TMainForm.MarkMenuItemClick(Sender: TObject);
+begin
+
+end;
+
+procedure TMainForm.QueryMenuItemClick(Sender: TObject);
+begin
+
+end;
+
+procedure TMainForm.NotifyMenuItemClick(Sender: TObject);
+begin
+
+end;
+
 procedure TMainForm.CopyMenuItemClick(Sender: TObject);
+begin
+
+end;
+
+procedure TMainForm.ArchiveMenuItemClick(Sender: TObject);
 begin
 
 end;
@@ -111,6 +187,16 @@ begin
 
 end;
 
+procedure TMainForm.QuitMenuItemClick(Sender: TObject);
+begin
+
+end;
+
+procedure TMainForm.RecallMenuItemClick(Sender: TObject);
+begin
+
+end;
+
 procedure TMainForm.RedoActionItemExecute(Sender: TObject);
 begin
 
@@ -122,6 +208,16 @@ begin
 end;
 
 procedure TMainForm.SelectAllMenuItemClick(Sender: TObject);
+begin
+
+end;
+
+procedure TMainForm.SignVerifyMenuItemClick(Sender: TObject);
+begin
+
+end;
+
+procedure TMainForm.TrayMenuItemClick(Sender: TObject);
 begin
 
 end;
